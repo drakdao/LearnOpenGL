e3a2a3918649b5ecc645c0ff8b1da6ee8dfd34a0
Nikhil Taneja <taneja.nikhil03@gmail.com>
2020-05-11 20:11:54 +0530

    uniform for CPU - GPU data communication

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -142,6 +142,7 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSwapInterval(1);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -186,6 +187,13 @@ int main()
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
+
+    GLCall( int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+
+    float r = 0.0f;
+    float increment = 0.05f;
+    
     
     // render loop
     // -----------
@@ -200,8 +208,16 @@ int main()
         GLCall(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
         GLCall(glBindVertexArray(vao));
+        
+        GLCall(glUniform4f(location, r, 0.5f, 0.2f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
+        if (r > 1.0f)
+            increment = -.05f;
+        else if (r < 0.0f)
+            increment = .05f;
+
+        r += increment;
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
